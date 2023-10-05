eefa0a625ef57b8b531597fbe968f0056a81e3c6
thirumala reddy <thirumalareddy797@gmail.com>
2023-10-05 18:11:47 +0530

    first python class

---
+++ class1.py
@@ -0,0 +1,32 @@
+"""
+Python:
+---------
+python is dynamic, interpretor, scripting,functional,
+        object oriented and high level programming language
+"""
+
+
+print("hellow world")
+
+
+# comment:
+    # - comment it wont execute python interpretor
+    
+
+# variables
+    # declaring namings or identification
+
+x = 20 #int
+y = 20.2 #float
+z = "thirumala" # string
+
+print("integer:",x)
+print("float:",y)
+print("string:",z)
+
+# for checking data type by using type keyword
+print("type of x is:",type(x))
+
+
+print("sum of x and y:",x+y)
+
