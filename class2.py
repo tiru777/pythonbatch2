78b08b8ac786405a6f86b7a70a2e51e20094042c
thirumala reddy <thirumalareddy797@gmail.com>
2023-10-06 17:35:39 +0530

    type casting

---
+++ class2.py
@@ -0,0 +1,80 @@
+# Type Casting: COnvertion from one data type to other data type
+"""
+# int ==> float
+    # ex: 20 ==>20.0
+
+x = 30
+y = float(x)#30.0
+print(y)
+print(type(y))
+"""
+"""
+# float ==> int
+
+y = 20.2
+print(type(y))
+yy = int(y)
+print(yy)
+print(type(yy))
+
+"""
+"""
+# int ==> str
+x = 30
+print("type of ",type(x),x)
+y = str(x) # "30"
+print("type of ",type(y),y)
+"""
+
+'''
+#str ==> int
+x = "20"
+print("type of ",x,type(x))
+y = int(x)
+print("type of ",y,type(y))
+
+'''
+#print: python function: it used for print the sepcified value
+"""
+# syntax
+print("represent",value)
+"""
+print("my value is",20)
+x = 20
+print("my value is",x)
+
+x = 10
+y = 20
+print("sum of x and y",x+y)
+z = "201"
+xx = "500"
+
+# sum ==> int+int,str+str, int+float but int and str ,float and str not possible
+#print("sum of x and z",x+z) # 
+zz = int(z)
+print("sum of z and zz",x+zz)
+
+print("sum of z and xx",z+xx)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
