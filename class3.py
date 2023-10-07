e2e2348f8aed211f849e64933f2760f8d4108835
thirumala reddy <thirumalareddy797@gmail.com>
2023-10-07 16:38:48 +0530

    Operators

---
+++ class3.py
@@ -0,0 +1,109 @@
+# operators
+
+# arithimetic operators: +, -,*, /,
+                    #//(floor division means it will eliminate decimal value),% 
+"""
+x = 101
+y = 20
+
+print("sum of x and y:",x+y)#30
+print("diffrence of x and y:",x-y)#-10
+print("multi of x and y:",x*y)#200
+print("division of x and y:",x/y)#0.5
+print("floor division of x and y:",x//y)#0
+print("modular of x and y:",x%y)#1
+"""
+
+# comparative operators: ==, !=
+"""
+x=10
+y = 20
+print(x==y)# False
+print(x!=y)# True
+"""
+
+# relational operators: >,<,>=,<=
+
+"""
+x=10
+y = 20
+print(x>y)# False
+print(x<y)# True
+print(x>=y) #False
+print(x<=y) # True
+
+"""
+
+# logical operators: and(T and T=T), or(T or F == T) , not(opposite)
+
+x = 20
+y = 10
+z = 6
+"""
+print(x>y and y<z) # True and False ==> False
+
+print(x>y or y<z) # True or False ==> True
+
+print(not(x>y)) # True           ==> False
+
+"""
+
+# Membership operators: in, not in
+
+"""
+x = "akhil gurrramkonda"
+
+print("h" in x)# True
+print("h" not in x)# False
+"""
+
+# identity operators: is , is not
+"""
+x = 20
+print(id(x)) #1441885979472
+y = 30
+print(id(y))# 1441885979792
+
+print(x is y)
+print(x is not y)
+"""
+
+# incremnetal operators : +=,-=,*=,/=
+
+x = 20
+x = x+5
+print(x)#25
+
+y = 10
+y *=5  #y = y-5
+print(y)# 15
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
